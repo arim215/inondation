964af63bd957c2404ee4bcb41f5c88c0945da236
Ari Mandjelikian <arim215@users.noreply.github.com>
2020-02-27 15:05:03 -0500

    Create inondation.py

---
+++ inondation.py
@@ -0,0 +1,31 @@
+import urllib.request
+import re
+from lib import I2C_LCD_driver
+
+
+x=0
+link = 'https://geoegl.msp.gouv.qc.ca/adnv2/tableau-region-simple.php?id=13&type_rapport=ADMIN'
+
+with urllib.request.urlopen(link) as url:
+    s = url.read()
+
+
+search = str(s,"utf-8")
+
+search=search.strip("\r\t\n")
+search = re.findall("(Rivi.re des Prairies[\s<>/a-z='ÀéA-Zê()0-9.#\*,]*)",search)
+for i in search:
+        niveau = re.findall("[0-9 ]*,[0-9]{2}",i)
+        for y in niveau:
+                if x == 0:
+                        SIM = ("SIM debit : "+y+" m³/s")
+                elif x==1:
+                        debit = ("Debit: "+y+" m³/s")
+                else:
+                        niveau ("Niveau : "+y+" m (normal - 20m)")
+                x+=1
+
+print (SIM + "\n" + debit + "\n" + niveau + "\n")
+mylcd = I2C_LCD_driver.lcd()
+
+mylcd.lcd_display_string("Hello World!", 1)
