9116d173196a45779a71b7b59cea039cb09d81c7
Ari Mandjelikian <arim215@users.noreply.github.com>
2020-02-27 15:48:41 -0500

    Update inondation.py

--- inondation.py
+++ inondation.py
@@ -1,31 +1,40 @@
 import urllib.request
-import re
+import re time
 from lib import I2C_LCD_driver
 
+sleep = 3600
 
 x=0
 link = 'https://geoegl.msp.gouv.qc.ca/adnv2/tableau-region-simple.php?id=13&type_rapport=ADMIN'
 
-with urllib.request.urlopen(link) as url:
-    s = url.read()
+try :
+    while (True):
+        
+        with urllib.request.urlopen(link) as url:
+            s = url.read()
 
 
-search = str(s,"utf-8")
+        search = str(s,"utf-8")
 
-search=search.strip("\r\t\n")
-search = re.findall("(Rivi.re des Prairies[\s<>/a-z='ÀéA-Zê()0-9.#\*,]*)",search)
-for i in search:
-        niveau = re.findall("[0-9 ]*,[0-9]{2}",i)
-        for y in niveau:
-                if x == 0:
-                        SIM = ("SIM debit : "+y+" m³/s")
-                elif x==1:
-                        debit = ("Debit: "+y+" m³/s")
-                else:
-                        niveau ("Niveau : "+y+" m (normal - 20m)")
-                x+=1
+        search=search.strip("\r\t\n")
+        search = re.findall("(Rivi.re des Prairies[\s<>/a-z='ÀéA-Zê()0-9.#\*,]*)",search)
+        for i in search:
+                niveau = re.findall("[0-9 ]*,[0-9]{2}",i)
+                for y in niveau:
+                        if x == 0:
+                                SIM = ("SIM debit : "+y+" m³/s")
+                        elif x==1:
+                                debit = ("Debit: "+y+" m³/s")
+                        else:
+                                niveau ("Niveau : "+y+" m (normal - 20m)")
+                        x+=1
 
-print (SIM + "\n" + debit + "\n" + niveau + "\n")
-mylcd = I2C_LCD_driver.lcd()
+        print (SIM + "\n" + debit + "\n" + niveau + "\n")
+        mylcd = I2C_LCD_driver.lcd()
+        mylcd.lcd_display_string("Hello World!", 1)
+        time.sleep(5)
+        #time.sleep(sleep)
 
-mylcd.lcd_display_string("Hello World!", 1)
+except KeyboardInterrupt:
+    print("\nCtrl-C pressed")
+    mylcd.lcd_clear()
